c24eb83bac497f90cb90cff753d5d62e074e8997
anubhav <arora.anubhav93@gmail.com>
2021-05-09 20:38:47 +0530

    Audiofy : Inital project setup, added viewbinding, updated to latest library version

---
+++ Audiofy/app/src/main/java/com/daakknights/audiofy/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.daakknights.audiofy
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.daakknights.audiofy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        setUpNav()
+    }
+
+    private fun setUpNav() {
+        val navView: BottomNavigationView = _binding.bnvNavigation
+
+        val navController = findNavController(R.id.fHostFragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
